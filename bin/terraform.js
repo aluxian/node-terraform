138268b2ea65a0d12c3284a8f7a4b6c7db7d7ec3
Alexandru Rosianu <me@aluxian.com>
2025-09-07 16:19:47 +0300

    cli script

--- bin/terraform.js
+++ bin/terraform.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-import "../src/cli.js";
+
+import { spawn } from "child_process";
+import { resolveTerraformBinary } from "../src/binaryResolver.js";
+
+const args = process.argv.slice(2);
+
+try {
+  // Use the new esbuild-style binary resolution system
+  // This tries platform packages first, then falls back to download
+  const terraformPath = await resolveTerraformBinary();
+
+  const terraform = spawn(terraformPath, args, {
+    stdio: [process.stdin, process.stdout, process.stderr],
+  });
+
+  terraform.on("close", (code) => process.exit(code || undefined));
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
